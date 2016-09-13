4bd4bb4cc12d9dbe74a5d24e6d0fd6e84c18e164
Nivesh Singh <nivesh2@gmail.com>
2016-09-14 01:49:55 +0530

    word definition display updated

--- controllers/operations.js
+++ controllers/operations.js
@@ -1,9 +1,22 @@
 'use strict';
 
-const wordnik = require('../api/wordnik.api');
+const wordnik = require('../helpers/wordnik.api');
 const output = require('../config/output');
+const format = require('../helpers/format.helper');
 
 module.exports = (function(){
+
+    //private methods
+    var printDefinition = function(input){
+        Object.keys(input).forEach((v)=>{
+            console.log(`from: ${v} \n`);
+
+            input[v].forEach((v)=>{
+               console.log(` - ${v}`);
+            });
+            console.log('--------------------------------------------\n')
+        });
+    }
 
     //public exposed methods
     const that ={};
@@ -13,10 +26,13 @@ module.exports = (function(){
 
         wordnik.getDefinition(word,function(err,json){
             if(err) return console.log('Error while fetching Definition from wordnik API',err);
-            console.log(json);
+
+            console.log(`Definitions: ${word} \n`);
+            printDefinition(format.definition(json));
         });
 
     };
+    
 
     return that;
 
