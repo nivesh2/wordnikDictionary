33c63751c551eb4d266d5bc6c2fd873ba437ae5b
Nivesh Singh <nivesh2@gmail.com>
2016-09-21 10:22:58 +0530

    api_key removed
    Developers need to use there own personal API key. 
    Thanks!

--- config/config.js
+++ config/config.js
@@ -2,5 +2,5 @@
 
 module.exports = {
     api_host : 'http://api.wordnik.com:80/v4/',
-    api_key : 'afdc14bbd0019f4ad7008085b3e0a9c687c6930acbe163510',
+    api_key : 'YOUR__WORDNIK__API__KEY',
 };
